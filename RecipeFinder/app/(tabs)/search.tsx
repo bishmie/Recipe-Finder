87614199dad7a10da92d73d5da5fd74f2cb7c0e0
bishmie <bishmimalshi@gmail.com>
2025-09-04 11:47:51 +0530

    refactor(search): replace placeholder with SearchScreen component

--- RecipeFinder/app/(tabs)/search.tsx
+++ RecipeFinder/app/(tabs)/search.tsx
@@ -1,22 +1,5 @@
-import { View, Text, StyleSheet } from 'react-native';
+import SearchScreen from '../../components/ui/screen/searchScreen';
 
 export default function Search() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Search Screen (Coming Soon)</Text>
-    </View>
-  );
+  return <SearchScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  text: {
-    fontSize: 18,
-    textAlign: 'center',
-  },
-});
