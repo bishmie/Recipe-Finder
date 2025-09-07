d4ad6b2dd94f0e9707622e127f440d97fc6615d3
bishmie <bishmimalshi@gmail.com>
2025-09-07 16:35:05 +0530

    Remove unnecessary View wrapper and React import since they're not needed for the RecipeScreen component

--- RecipeFinder/app/recipe/[id].tsx
+++ RecipeFinder/app/recipe/[id].tsx
@@ -1,11 +1,5 @@
-import React from 'react';
-import { View } from 'react-native';
 import RecipeScreen from '../../components/ui/screen/recipeScreen';
 
 export default function RecipeDetailPage() {
-  return (
-    <View style={{ flex: 1 }}>
-      <RecipeScreen />
-    </View>
-  );
+  return <RecipeScreen />;
 }
